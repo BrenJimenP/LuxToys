4d8518be5f46541cbf632d859656d900fc9e4650
Jimenez-Phillips <BJIMEN19@depaul.edu>
2023-04-05 18:40:12 -0500

    updated contact

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var contactRouter = require('./routes/contact');
+
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
